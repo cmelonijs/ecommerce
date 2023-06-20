2c1765ce328fae23ec3ff018cbd0975888ed6303
cmeloni <carlo.meloni@abinsula.com>
2023-06-20 10:40:14 +0200

    feat: adding a navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import logo from "./logo.svg"
 import "./App.css"
+import Main from "./components/main/Main"
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+  return <Main></Main>
 }
 
 export default App
---
+++ src/components/main/Main.tsx
@@ -0,0 +1,11 @@
+import Navbar from "../navbar/Navbar"
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Main
